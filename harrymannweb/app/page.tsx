0115c50db3d7070cfcd58a0d21643846dc6e0ff3
Navjot Saran <fly2962@hotmail.com>
2023-09-19 00:46:29 -0600

    initial setup complete

---
+++ harrymannweb/app/page.tsx
@@ -0,0 +1,20 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className="grid grid-cols-2 grid-rows-2 gap-x-1/2 gap-y-1/2">
+      <div className=" relative bg-slate-300">
+        business
+      </div>
+      <div className=" relative">
+        business
+      </div>
+      <div className=" relative">
+        business
+      </div>
+      <div className=" relative">
+        business
+      </div>
+    </main>
+  )
+}
